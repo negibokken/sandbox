8a81ae8c5aa1844d2f5984d0d0e3d77cdad0f09d
negibokken <negibokken@gmail.com>
2019-03-30 13:38:23 +0900

    Add chapter 1 problem 1

---
+++ programming-practice/chapter1/problem1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+bool HasDuplicatedChar(string str);
+
+int main(int argc, char *argv[]) {
+  string str = "hello world";
+  if (argc <= 1) {
+    cout << "./" << argv[0] << " <string>" << endl;
+    exit(1);
+  }
+  bool result = HasDuplicatedChar(argv[1]);
+  return result;
+}
+
+bool HasDuplicatedChar(string str) {
+  for (int i = 0; i < str.length(); i++) {
+    for (int j = i + 1; j < str.length(); j++) {
+      if (str[i] == str[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
