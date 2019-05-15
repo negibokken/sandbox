1334ee80cc090750eef52c0556a654a591727af2
negibokken <negibokken@gmail.com>
2019-05-15 20:37:18 +0900

    Add chapter 2 ex 13

---
+++ programming-challenge-book/chapter2/ex13/main.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+using namespace std;
+
+#define MAX_N 10000
+#define MAX_W 10000
+
+int n, W;
+int w[MAX_W], v[MAX_N];
+int dp[MAX_N][MAX_W];
+
+void solve()
+{
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j <= W; j++) {
+      for (int k = 0; k * w[i] <= j; k++) {
+        dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - k * w[i]] + k * v[i]);
+      }
+    }
+  }
+  printf("%d\n", dp[n][W]);
+}
+
+int main(void)
+{
+  scanf("%d %d\n", &n, &W);
+  REP(i, n) scanf("(%d, %d) ", &w[i], &v[i]);
+  solve();
+  return 0;
+}
