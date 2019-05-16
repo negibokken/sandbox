3f5100d4f84d6cbcad3d8f0f1f3ac1aeb8c891df
negibokken <negibokken@gmail.com>
2019-05-16 22:43:49 +0900

    Update ex 15

--- programming-challenge-book/chapter2/ex15/main.cc
+++ programming-challenge-book/chapter2/ex15/main.cc
@@ -15,19 +15,26 @@ int n;
 int a[MAX_N], m[MAX_N];
 int K;
 
-bool dp[MAX_N + 1][MAX_K + 1];
+int dp[MAX_K + 1];
 
 void solve()
 {
-  dp[0][0] = true;
+  memset(dp, -1, sizeof(dp));
+  dp[0] = 0;
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j <= K; ++j) {
-      for (int k = 0; k <= m[i] && k * a[i] <= j; ++k) {
-        dp[i + 1][j] |= dp[i][j - k * a[i]];
+      if (dp[j] >= 0) {
+        dp[j] = m[i];
+      }
+      else if (j < a[i] || dp[j - a[i]] <= 0) {
+        dp[j] = -1;
+      }
+      else {
+        dp[j] = dp[j - a[i]] - 1;
       }
     }
   }
-  if (dp[n][K])
+  if (dp[K] >= 0)
     printf("Yes\n");
   else
     printf("No\n");
