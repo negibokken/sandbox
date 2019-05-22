25177e4121e7df077ff373c662e9d24527b78856
negibokken <negibokken@gmail.com>
2019-05-23 06:41:33 +0900

    Add chapter 2 ex 20

--- programming-challenge-book/chapter2/ex20/main.cc
+++ programming-challenge-book/chapter2/ex20/main.cc
@@ -13,12 +13,32 @@ void myscanf(const char *fmt, int num, int *s) { REP(i,num) scanf(fmt, &s[i]); }
 int n, m, M;
 int a[MAX_N];
 
+int dp[MAX_N][MAX_N];
+
+void solve()
+{
+  for (int i = 0; i < n; ++i) {
+    dp[i][0] = 1;
+  }
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j <= m; ++j) {
+      if (j - 1 - a[i] >= 0) {
+        dp[i + 1][j] =
+            (dp[i + 1][j - 1] + dp[i][j] - dp[i][j - 1 - a[i]] + M) % M;
+      }
+      else {
+        dp[i + 1][j] = dp[i + 1][j - 1] + dp[i][j] % M;
+      }
+    }
+  }
+  printf("%d\n", dp[n][m]);
+}
+
 int main(void)
 {
   scanf("%d %d %d\n", &n, &m, &M);
   const char *fmt = "%d ";
   myscanf(fmt, n, a);
-  printf("%d %d %d\n", n, m, M);
-  REP(i, n) printf("%d\n", a[i]);
+  solve();
   return 0;
 }
