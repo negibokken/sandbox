2c3c77c315ccf2584ac15c9326e8507e4acdcb28
negibokken <negibokken@gmail.com>
2019-05-26 21:03:25 +0900

    Add question 1

---
+++ abc/abc128/question1/main.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int A, P;
+
+int main(void)
+{
+  scanf("%d %d", &A, &P);
+  printf("%d\n", (A * 3 + P) / 2);
+  return 0;
+}
---
+++ abc/abc128/question1/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 3
+1 3
+EOF
+
+## test case 2
+cat << EOF | try 0
+0 1
+EOF
+
+## test case 2
+cat << EOF | try 58
+32 21
+EOF
