bcd1b151e7fb25f9fd5dd79315d9f49e6ea0d5b8
negibokken <negibokken@gmail.com>
2019-05-26 11:56:15 +0900

    Add chapter 2 ex 23

---
+++ programming-challenge-book/chapter2/ex23/main.cc
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct node {
+  int val;
+  node *lch, *rch;
+};
+
+node *insert(node *p, int x)
+{
+  if (p == NULL) {
+    node *q = new node;
+    q->val = x;
+    q->lch = q->rch = NULL;
+    return q;
+  }
+  else {
+    if (x < p->val)
+      p->lch = insert(p->rch, x);
+    else
+      p->rch = insert(p->rch, x);
+    return p;
+  }
+}
+
+bool find(node *p, int x)
+{
+  if (p == NULL)
+    return false;
+  else if (x == p->val)
+    return true;
+  else if (x < p->val)
+    return find(p->lch, x);
+  else
+    return find(p->rch, x);
+}
+
+node *remove(node *p, int x)
+{
+  if (p == NULL)
+    return NULL;
+  else if (x < p->val)
+    p->lch = remove(p->lch, x);
+  else if (x > p->val)
+    p->rch = remove(p->rch, x);
+  else if (p->lch == NULL) {
+    node *q = p->rch;
+    delete p;
+    return q;
+  }
+  else if (p->lch->rch == NULL) {
+    node *q = p->lch;
+    q->rch = p->rch;
+    delete p;
+    return q;
+  }
+  else {
+    node *q;
+    for (q = p->lch; q->rch->rch != NULL; q = q->rch)
+      ;
+    node *r = q->rch;
+    q->rch = r->lch;
+    r->lch = p->lch;
+    r->rch = p->rch;
+    delete p;
+    return r;
+  }
+  return p;
+}
+
+int main(void)
+{
+  node *root = NULL;
+
+  cout << find(root, 1) << endl;
+  root = insert(root, 1);
+  cout << find(root, 1) << endl;
+  root = remove(root, 1);
+  cout << find(root, 1) << endl;
+  return 0;
+}
