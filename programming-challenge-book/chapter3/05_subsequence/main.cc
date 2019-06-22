e3efcc53202d502fa81cf01517dc0b7503bbddfd
negibokken <negibokken@gmail.com>
2019-06-22 14:24:43 +0900

    Update chapter 3 by using measuring-worm algorithm

--- programming-challenge-book/chapter3/05_subsequence/main.cc
+++ programming-challenge-book/chapter3/05_subsequence/main.cc
@@ -30,19 +30,22 @@ ll sum[MAX_N];
 
 void solve()
 {
-  for (int i = 0; i < n; i++) {
-    sum[i + 1] = sum[i] + a[i];
-  }
-
-  if (sum[n] < S) {
-    printf("0\n");
-    return;
-  }
-
-  int res = n;
-  for (int s = 0; sum[s] + S <= sum[n]; s++) {
-    int t = lower_bound(sum + s, sum + n, sum[s] + S) - sum;
+  int res = n + 1;
+  int s = 0, t = 0, sum = 0;
+  for (;;) {
+    while (t < n && sum < S) {
+      sum += a[t++];
+    }
+    if (sum < S) break;
     res = min(res, t - s);
+    sum -= a[s++];
+    while (s < t && sum > S) {
+      res = min(res, t - s);
+      sum -= a[s++];
+    }
+  }
+  if (res > n) {
+    res = 0;
   }
   printf("%d\n", res);
 }
