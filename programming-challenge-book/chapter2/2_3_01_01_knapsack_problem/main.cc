b4a88dd164c9c95e687e0ffba907afed448d2d48
negibokken <negibokken@gmail.com>
2019-06-29 20:53:29 +0900

    Add 2-3 01 01-knapsack problem

---
+++ programming-challenge-book/chapter2/2_3_01_01_knapsack_problem/main.cc
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+// clang-format on
+
+int n, W;
+int w[MAX_N], v[MAX_N];
+
+int rec(int i, int j)
+{
+  int res;
+  if (i == n) {
+    res = 0;
+  }
+  else if (j < w[i]) {
+    res = rec(i + 1, j);
+  }
+  else {
+    res = max(rec(i + 1, j), rec(i + 1, j - w[i]) + v[i]);
+  }
+  return res;
+}
+
+void solve() { printf("%d\n", rec(0, W)); }
+
+int main(void)
+{
+  cin >> n >> W;
+  REP(i, n) cin >> w[i] >> v[i];
+  solve();
+  return 0;
+}
