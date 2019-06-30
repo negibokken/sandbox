42edecd95a8ec974c80b725c0e8c0554655e3fee
negibokken <negibokken@gmail.com>
2019-06-30 11:57:50 +0900

    Add abc132 question 1

---
+++ atcoder/abc/abc132/question1/main.cc
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+// clang-format on
+
+int main(void)
+{
+  map<char, int> m;
+  char s[5];
+  scanf("%s", s);
+  for (int i = 0; i < 4; i++) m[s[i]]++;
+
+  bool f = true;
+  for (auto i = m.begin(); i != m.end(); i++) {
+    if (i->second != 2) {
+      f = false;
+    }
+  }
+  printf("%s\n", (f ? "Yes" : "No"));
+  return 0;
+}
