8b237a5f2317f8c4f3bc13fb62f4c3fc3bb2d853
negibokken <negibokken@gmail.com>
2019-07-06 13:42:39 +0900

    Add dynamic programming practice

---
+++ programming-challenge-book/chapter2/retake/temp2/main.cc
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+// clang-format on
+
+int n, W;
+int w[MAX_N], v[MAX_N];
+int dp[MAX_N + 1][MAX_N + 1];
+
+void solve()
+{
+  REP(i, n) REP(j, n) dp[i][j] = 0;
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= W; j++) {
+      if (j < w[i]) {
+        dp[i + 1][j] = dp[i][j];
+      }
+      else {
+        dp[i + 1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
+      }
+    }
+  }
+  printf("%d\n", dp[n][W]);
+}
+
+int main(void)
+{
+  cin >> n >> W;
+  REP(i, n) cin >> w[i] >> v[i];
+  solve();
+  return 0;
+}
---
+++ programming-challenge-book/chapter2/retake/temp2/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 7
+4 5
+2 3
+1 2
+3 4
+2 2
+EOF
+
