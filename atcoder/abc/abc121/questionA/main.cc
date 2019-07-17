7c5632152fa979590348aa112d8b879c64e5ba59
negibokken <negibokken@gmail.com>
2019-07-17 23:41:51 +0900

    Add abc 121 question A

---
+++ atcoder/abc/abc121/questionA/main.cc
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+// clang-format on
+int H, W;
+int h, w;
+
+int main(void)
+{
+  cin >> H >> W;
+  cin >> h >> w;
+  cout << W * H - (h * W + w * H - (h * w)) << endl;
+  return 0;
+}
---
+++ atcoder/abc/abc121/questionA/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 1
+3 2
+2 1
+EOF
+
+## test case 2
+cat << EOF | try 6
+5 5
+2 3
+EOF
+
+## test case 2
+cat << EOF | try 0
+2 4
+2 4
+EOF
