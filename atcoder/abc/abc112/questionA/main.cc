203cd0fdd1f28ee1c1fb7bfa322dafa6850c0713
negibokken <negibokken@gmail.com>
2019-08-06 23:05:23 +0900

    Add abc 112 question A

---
+++ atcoder/abc/abc112/questionA/main.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+// clang-format on
+
+int main(void)
+{
+  int N;
+  int A, B;
+  cin >> N >> A >> B;
+  if (N == 1) {
+    cout << "Hello World" << endl;
+  }
+  else if (N == 2) {
+    cout << A + B << endl;
+  }
+  return 0;
+}
