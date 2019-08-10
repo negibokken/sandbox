98a49c2384cbb576533e8444f0ea1703b97ae0b8
negibokken <negibokken@gmail.com>
2019-08-10 12:35:03 +0900

    Add abc 110 question A

---
+++ atcoder/abc/abc110/questionA/main.cc
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+// clang-format on
+
+int A, B, C;
+
+int main(void)
+{
+  cin >> A >> B >> C;
+  int ma = max({A, B, C});
+  int ans = ma * 10 + (A + B + C - ma);
+  cout << ans << endl;
+  return 0;
+}
