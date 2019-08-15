5e834b70608efe490634f1f25d44d349d1629d75
negibokken <negibokken@gmail.com>
2019-08-15 16:40:30 +0900

    Add abc 128 question 3

--- atcoder/abc/abc128/question3/main.cc
+++ atcoder/abc/abc128/question3/main.cc
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 #include <bitset>
 #include <iostream>
@@ -8,61 +9,46 @@ using namespace std;
 // clang-format off
 #define MAX_N 10
 #define REP(i, N) for (int i = 0; i < (int)(N); ++i)
-void myscanf(const char *fmt, int num, int *s, int *v) { REP(i,num) scanf(fmt, &s[i], &v[i]); }
-void myscanf(const char *fmt, int num, int *s) { REP(i,num) scanf(fmt, &s[i]); }
 // clang-format on
 
 int N, M;
 
 int k[MAX_N + 1];
 int p[MAX_N + 1];
-int dp[MAX_N + 1][MAX_N + 1];
+int s[MAX_N + 1];
 
 int main(void)
 {
   cin >> N >> M;
-
-  // それぞれの電球がどのビット列でつくか保持
-  vector<int> a(N);
-
-  for (int i = 0; i < M; ++i) {
+  REP(i, M)
+  {
     int k;
     cin >> k;
-    for (int j = 0; j < k; ++j) {
-      int s;
-      cin >> s;
-      s--;
-      // sw3, sw2, sw1 の並びのビット列で表現される
-      // e.g 101, 001
-      a[i] |= (1 << s);
+    REP(j, k)
+    {
+      int ss;
+      cin >> ss;
+      s[i] += (int)pow(2, ss - 1);
     }
   }
-
-  for (int i = 0; i < M; ++i) {
-    cout << std::bitset<8>(a[i]) << endl;
-  }
-
-  vector<int> p(M);
-  for (int i = 0; i < M; ++i) {
-    int x;
-    cin >> x;
-    p[i] = x;
-  }
+  REP(i, M) cin >> p[i];
 
   int ans = 0;
-  for (int s = 0; s < (1 << N); ++s) {
-    for (int i = 0; i < M; ++i) {
-      int t = 0;
-      for (int j = 0; j < N; ++j) {
-        if (s >> j & 1) {
-          t++;
-        }
+  for (int i = 0; i < (1 << N); i++) {
+    int onLight = 0;
+    for (int j = 0; j < M; j++) {
+      int no1 = s[j] & i;
+      int cnt = 0;
+      // count # 1
+      while (no1 > 0) {
+        if (no1 & 1) cnt++;
+        no1 >>= 1;
       }
-      if (t % 2 == p[i]) {
-        ans++;
-      }
+      if (cnt % 2 == p[j]) onLight++;
     }
+    if (onLight == M) ans++;
   }
+
   cout << ans << endl;
 
   return 0;
