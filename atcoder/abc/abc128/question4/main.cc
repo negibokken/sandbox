6d55ed4fd48f65aedc852113eefa2e691e748715
negibokken <negibokken@gmail.com>
2019-08-15 20:21:03 +0900

    Add abc 128 question 4

--- atcoder/abc/abc128/question4/main.cc
+++ atcoder/abc/abc128/question4/main.cc
@@ -3,12 +3,12 @@
 #include <queue>
 using namespace std;
 
+typedef long long ll;
+
 // Self settings
 // clang-format off
 #define MAX_N 10000
 #define REP(i, N) for (int i = 0; i < (int)(N); ++i)
-void myscanf(const char *fmt, int num, int *s, int *v) { REP(i,num) scanf(fmt, &s[i], &v[i]); }
-void myscanf(const char *fmt, int num, int *s) { REP(i,num) scanf(fmt, &s[i]); }
 // clang-format on
 
 int N, K;
@@ -16,14 +16,54 @@ int V[MAX_N];
 
 int main(void)
 {
-  deque<int> dq;
+  deque<int> dqe;
   cin >> N >> K;
   for (int i = 0; i < N; ++i) {
     cin >> V[i];
-    dq.push_back(V[i]);
+    dqe.push_back(V[i]);
   }
-  for (int i = 0; i < N; ++i) {
-    cout << V[i];
+
+  ll ans = 0;
+
+  for (int a = 0; a <= min(N, K); a++) {
+    deque<int> dq = dqe;
+    priority_queue<int, vector<int>, greater<int>> que;
+    int aa = a;
+    while (aa > 0 && !dq.empty()) {
+      que.push(dq.front());
+      dq.pop_front();
+      aa--;
+    }
+
+    for (int b = 0; b <= min(N, K) - a; b++) {
+      deque<int> dqq = dq;
+      priority_queue<int, vector<int>, greater<int>> qu = que;
+      int bb = b;
+      while (bb > 0 && !dqq.empty()) {
+        qu.push(dqq.back());
+        dqq.pop_back();
+        bb--;
+      }
+
+      for (int k = 0; k <= K - (a + b); k++) {
+        int kk = k;
+        priority_queue<int, vector<int>, greater<int>> q = qu;
+        while (kk > 0) {
+          if (!q.empty() && q.top() < 0) {
+            q.pop();
+          }
+          kk--;
+        }
+        ll sum = 0;
+        while (!q.empty()) {
+          sum += q.top();
+          q.pop();
+        }
+        ans = max(ans, sum);
+      }
+    }
   }
+  cout << ans << endl;
+
   return 0;
 }
