668e49476b08b3c0de2b2baafb29bec3fac09d17
negibokken <negibokken@gmail.com>
2019-08-31 17:20:28 +0900

    Add abc099 questionA

---
+++ atcoder/abc/abc099/questionA/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try ABC
+999
+EOF
+
+## test case 2
+cat << EOF | try ABD
+1000
+EOF
+
+## test case 2
+cat << EOF | try ABD
+1481
+EOF
