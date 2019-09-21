79d5f0d52939d310ec7fdaf25be7a06cf567a845
negibokken <negibokken@gmail.com>
2019-09-21 16:43:00 +0900

    Add abc090 questionA

---
+++ atcoder/abc/abc090/questionA/main.cc
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 3
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1 << 31;
+// clang-format on
+
+char c[MAX_N][MAX_N];
+
+int main(void)
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  REP(i, 3) REP(j, 3) { cin >> c[i][j]; }
+
+  REP(i, 3) { cout << c[i][i]; }
+  cout << endl;
+
+  return 0;
+}
---
+++ atcoder/abc/abc090/questionA/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try abc
+ant
+obe
+rec
+EOF
+
+## test case 2
+cat << EOF | try ean
+edu
+cat
+ion
+EOF
