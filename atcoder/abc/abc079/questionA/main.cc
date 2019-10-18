deb48766db99175d3952d3136d37034b8ff8a412
negibokken <negibokken@gmail.com>
2019-10-18 20:18:43 +0900

    Add abc079 questionA

---
+++ atcoder/abc/abc079/questionA/main.cc
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1 << 31;
+// clang-format on
+
+int N;
+
+int main(void)
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cin >> N;
+
+  int m = 0, cnt = 0;
+  int prev = 100;
+  int n = N;
+  while (n > 0) {
+    int cur = n % 10;
+    if (prev == cur) {
+      cnt++;
+    }
+    else {
+      cnt = 1;
+    }
+    m = max(m, cnt);
+    prev = cur;
+    n /= 10;
+  }
+  if (m >= 3) {
+    cout << "Yes" << endl;
+  }
+  else {
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
