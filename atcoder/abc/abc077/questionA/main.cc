e73e10c3d3f4764a62a1ce6b0a346bb4823a98b1
negibokken <negibokken@gmail.com>
2019-10-22 20:39:56 +0900

    Add abc077 questionA

---
+++ atcoder/abc/abc077/questionA/main.cc
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1 << 31;
+// clang-format on
+
+char C[2][3];
+int main(void)
+{
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  REP(i, 2) REP(j, 3) cin >> C[i][j];
+
+  if (C[0][0] != C[1][2] || C[0][1] != C[1][1] || C[0][2] != C[1][0]) {
+    cout << "NO" << endl;
+    return 0;
+  }
+  cout << "YES" << endl;
+  return 0;
+}
---
+++ atcoder/abc/abc077/questionA/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try YES
+pot
+top
+EOF
+
+## test case 2
+cat << EOF | try NO
+tab
+bet
+EOF
+
+## test case 2
+cat << EOF | try NO
+eye
+eel
+EOF
