ad75093a4021d659835ded9e96b6884bbeab2c7e
negibokken <negibokken@gmail.com>
2019-12-04 03:12:06 +0900

    Add abc059 questionA

---
+++ atcoder/abc/abc059/questionA/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try ABC
+atcoder beginner contest
+EOF
+
+## test case 2
+cat << EOF | try RRN
+resident register number
+EOF
+
+## test case 2
+cat << EOF | try KNN
+k nearest neighbor
+EOF
+
+## test case 2
+cat << EOF | try ALC
+async layered coding
+EOF
