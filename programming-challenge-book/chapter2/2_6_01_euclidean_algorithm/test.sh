93a4113fa017e8492c9c817845998298ad131d44
negibokken <negibokken@gmail.com>
2020-01-30 01:05:14 +0900

    Add chapter2 2_6_01_euclidean_algorithm

---
+++ programming-challenge-book/chapter2/2_6_01_euclidean_algorithm/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 3
+1 11
+5 3
+EOF
+
