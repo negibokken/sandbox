82d3797ddf9ca9b6b85e120999c4e20914087d7c
negibokken <negibokken@gmail.com>
2020-01-30 08:00:50 +0900

    Add temp practice1

---
+++ programming-challenge-book/temp/practice1/main.cc
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+// clang-format on
+
+int n, k;
+int a[MAX_N];
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  cin >> n >> k;
+  REP(i, n) cin >> a[i];
+
+  int ans = 0;
+  int left = 0, right = n;
+  while (right - left > 1) {
+    int mid = (left + right) / 2;
+    if (a[mid] >= k) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+  cout << right << endl;
+
+  return 0;
+}
---
+++ programming-challenge-book/temp/practice1/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 1
+2 3 3 5 6
+EOF
