5e85954dee8ca01f7a0025e2f8051af5050eadc9
negibokken <negibokken@gmail.com>
2020-02-15 13:09:08 +0900

    Add part2 stable_sort

---
+++ algorithm-and-data-structure-for-programming-contest/part2/stable_sort/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo $input | ./main`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try
+5
+H4 C9 S4 D2 C3
+EOF
+
