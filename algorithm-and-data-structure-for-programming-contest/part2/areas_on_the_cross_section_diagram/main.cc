dbefb376b90f770805e7647a39af173dc353a71e
negibokken <negibokken@gmail.com>
2020-02-17 21:07:03 +0900

    Add part2 areas_on_the_cross_section_diagram

--- algorithm-and-data-structure-for-programming-contest/part2/areas_on_the_cross_section_diagram/main.cc
+++ algorithm-and-data-structure-for-programming-contest/part2/areas_on_the_cross_section_diagram/main.cc
@@ -39,38 +39,19 @@ int main(void) {
   ios::sync_with_stdio(false);
 
   stack<int> S1;
-  stack<P> S2;
-  char ch;
+
   int sum = 0;
-  for (int i = 0; cin >> ch; i++) {
-    if (ch == '\\')
-      S1.push(i);
-    else if (ch == '/' && S1.size() > 0) {
+  char c;
+  for (int i = 0; cin >> c; i++) {
+    if (c == '\\') S1.push(i);
+    if (c == '/' && S1.size() > 0) {
       int j = S1.top();
       S1.pop();
       sum += i - j;
       int a = i - j;
-      while (S2.size() > 0 && S2.top().first > j) {
-        a += S2.top().second;
-        S2.pop();
-      }
-      S2.push(P(j, a));
     }
   }
-
-  vector<int> ans;
-  while (S2.size() > 0) {
-    ans.push_back(S2.top().second);
-    S2.pop();
-  }
-  reverse(ans.begin(), ans.end());
   cout << sum << endl;
-  cout << ans.size();
-  for (int i = 0; i < ans.size(); i++) {
-    cout << " ";
-    cout << ans[i];
-  }
-  cout << endl;
 
   return 0;
 }
