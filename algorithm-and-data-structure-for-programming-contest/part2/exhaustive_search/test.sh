dfbd257a1b332050f84ef8d5ca8038f1369b6266
negibokken <negibokken@gmail.com>
2020-02-18 21:05:07 +0900

    Add part2 exhaustive_search

---
+++ algorithm-and-data-structure-for-programming-contest/part2/exhaustive_search/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo $actual`
+  expected=`echo $expected`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try "no no yes yes"
+5
+1 5 7 10 21
+4
+2 4 17 8
+EOF
+
