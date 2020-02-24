b45e5f2eb59f3219ca8be7f4968698b349a238ca
negibokken <negibokken@gmail.com>
2020-02-24 22:30:20 +0900

    Add abc156 questionC

---
+++ atcoder/abc/abc156/questionC/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo $actual`
+  expected=`echo $expected`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 5
+2
+1 4
+EOF
+
+## test case 2
+cat << EOF | try 2354
+7
+14 14 2 13 56 2 37
+EOF
