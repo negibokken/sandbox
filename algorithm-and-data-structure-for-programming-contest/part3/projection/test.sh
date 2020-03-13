1e2fdf54dbdbbd9deb99edd28110137da784471d
negibokken <negibokken@gmail.com>
2020-03-14 04:08:10 +0900

    Add part3 projection

---
+++ algorithm-and-data-structure-for-programming-contest/part3/projection/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo $actual`
+  expected=`echo $expected`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try "3.120000000 4.160000000"
+0 0 3 4
+1
+2 5
+EOF
+
