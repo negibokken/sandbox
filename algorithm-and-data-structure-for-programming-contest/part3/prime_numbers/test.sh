1cea085290c61e18bd92e560b038719230081abf
negibokken <negibokken@gmail.com>
2020-03-15 22:08:12 +0900

    Add part3 prime_numbers

---
+++ algorithm-and-data-structure-for-programming-contest/part3/prime_numbers/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo $actual`
+  expected=`echo $expected`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 4
+6
+2
+3
+4
+5
+6
+7
+EOF
+
