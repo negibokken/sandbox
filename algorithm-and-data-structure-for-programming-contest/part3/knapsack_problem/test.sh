0456c8118b2b2453ef3275d7bd4b14173034ff63
negibokken <negibokken@gmail.com>
2020-03-15 15:12:03 +0900

    Add part3 knapsack_problem

---
+++ algorithm-and-data-structure-for-programming-contest/part3/knapsack_problem/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo $actual`
+  expected=`echo $expected`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 13
+4 5
+4 2
+5 2
+2 1
+8 3
+EOF
+
