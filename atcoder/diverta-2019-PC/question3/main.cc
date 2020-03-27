aedf852227043f7d26de81160c50c8319e39c2fd
negibokken <negibokken@gmail.com>
2020-03-27 23:01:07 +0900

    Add diverta-2019-PC question3

--- atcoder/diverta-2019-PC/question3/main.cc
+++ atcoder/diverta-2019-PC/question3/main.cc
@@ -1,100 +1,96 @@
-#include <stdio.h>
-#include <string.h>
 #include <algorithm>
-#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
 #include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
 
-int fact(int n);
-int nCr(int n, int r) { return fact(n) / (fact(r) * fact(n - r)); }
+typedef long long ll;
+typedef pair<int, int> P;
 
-// Returns factorial of n
-int fact(int n)
-{
-  int res = 1;
-  for (int i = 2; i <= n; i++) res = res * i;
-  return res;
-}
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
 
-int countAB(string s)
-{
-  int cnt;
-  for (string::iterator i = s.begin(); i != s.end(); ++i) {
-    if (*i == 'A') {
-      if (i + 1 != s.end() && *(i + 1) == 'B') ++cnt;
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+// clang-format on
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  ll N;
+  cin >> N;
+  vector<string> s(N);
+  vector<int> sab(N);
+  int nfb = 0, nba = 0, bab = 0;
+  REP(i, N) cin >> s[i];
+
+  for (int i = 0; i < N; i++) {
+    int cnt = 0;
+    if (s[i][0] == 'B' && s[i][s[i].size() - 1] == 'A') {
+      bab++;  // c1
+    } else if (s[i][s[i].size() - 1] == 'A') {
+      nba++;
+    } else if (s[i][0] == 'B') {
+      nfb++;
     }
-  }
-  return cnt;
-}
 
-int threeComb(int &ea, int &sb, int &both)
-{
-  int mi = min({ea, sb, both});
-  ea -= mi;
-  sb -= mi;
-  both -= mi;
-  return mi * 2;
-}
-
-int twoComb(int &ea, int &sb, int &both)
-{
-  array<int, 3> a = {ea, sb, both};
-  sort(a.begin(), a.end());
-  int mi = min(a[1], a[2]);
-  cout << a[1] << "|" << a[2] << endl;
-  return mi;
-}
-
-int main(void)
-{
-  int n;
-  scanf("%d", &n);
-
-  string s;
-
-  int ans = 0;
-  int sb = 0, ea = 0;  // num of start a & end b
-  int both = 0;
-
-  for (int i = 0; i < n; ++i) {
-    cin >> s;
-    int cnt = countAB(s);
-    ans += cnt;
-    if (s[0] == 'B' && *(s.end() - 1) == 'A') {
-      ++both;
+    for (int j = 0; j < s[i].size() - 1; j++) {
+      if (s[i][j] == 'A' && s[i][j + 1] == 'B') {
+        cnt++;
+      }
     }
-    else if (s[0] == 'B') {
-      ++sb;
-    }
-    else if (*(s.end() - 1) == 'A') {
-      ++ea;
-    }
+    sab[i] = cnt;
   }
 
-  printf("ans: %d\n", ans);
-  printf("ea: %d\n", ea);
-  printf("sb: %d\n", sb);
-  printf("both: %d\n", both);
-  int part = 0;
-  part += threeComb(ea, sb, both);
-  printf("part: %d\n", part);
+  ll ans = 0;
+  REP(i, N) ans += sab[i];
 
-  printf("ea: %d\n", ea);
-  printf("sb: %d\n", sb);
-  printf("both: %d\n", both);
-  part += twoComb(ea, sb, both);
-  printf("ea: %d\n", ea);
-  printf("sb: %d\n", sb);
-  printf("both: %d\n", both);
+  if (bab == 0) {
+    ans += min(nba, nfb);
+  } else {
+    if (nba + nfb > 0) {
+      ans += bab + min(nba, nfb);
+    } else if (nba + nfb == 0) {
+      ans += bab - 1;
+    }
+  }
 
-  // part = max({part, nCr((sb + i), (ea + (both - i))),
-  //             nCr((ea + i), (sb + (both - i)))});
-  // printf("ans: %d\n", ans);
-  // printf("ea: %d\n", ea);
-  // printf("sb: %d\n", sb);
-  // printf("both: %d\n", both);
-  // printf("part: %d\n", part);
+  cout << ans << endl;
 
-  printf("%d\n", ans + part);
   return 0;
 }
--- atcoder/diverta-2019-PC/question3/test.sh
+++ atcoder/diverta-2019-PC/question3/test.sh
@@ -4,10 +4,14 @@ try() {
   expected="$@"
   input=`cat -`
 
-  actual=`echo $input | ./main`
+  actual=`echo "$input" | ./main`
+
+  # actual=`echo $actual`
+  # expected=`echo $expected`
 
   if [ "$actual" = "$expected" ]; then
-    echo "$input => $actual"
+    # echo "$input => $actual"
+    echo "OK"
   else
     echo "==="
     echo "$input => $expected expected, but got $actual"
@@ -37,6 +41,7 @@ QVFABZ
 PRGKSPUNA
 EOF
 
+## test case 2
 cat << EOF | try 4
 7
 RABYBBE
