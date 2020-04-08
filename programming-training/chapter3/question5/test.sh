29110c2598138fa5033b8e3c88803ed05ca16e07
negibokken <negibokken@gmail.com>
2020-04-08 22:41:46 +0900

    Add chapter3 question5

---
+++ programming-training/chapter3/question5/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo $actual`
+  expected=`echo $expected`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 12 10 8 7 5 3 1
+7
+7 10 5 8 12 1 3
+EOF
+
