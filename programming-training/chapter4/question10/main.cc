ed9a9fefec6f18fa2293f822bb4cee930057fb22
negibokken <negibokken@gmail.com>
2020-04-17 02:14:28 +0900

    Add chapter4 question10

---
+++ programming-training/chapter4/question10/main.cc
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NULL), right(NULL) {} };
+// clang-format on
+
+Node* make_tree() {
+  Node* head = new Node(1);
+  head->left = new Node(2);
+  head->right = new Node(3);
+  head->left->left = new Node(4);
+  return head;
+}
+
+Node* make_tree2() {
+  Node* head = new Node(2);
+  head->left = new Node(4);
+  return head;
+}
+
+void getOrderString(Node* node, string& sb) {
+  if (node == NULL) {
+    sb.append("X");
+    return;
+  }
+  sb.append(to_string(node->data) + " ");
+  getOrderString(node->left, sb);
+  getOrderString(node->right, sb);
+}
+
+bool containesTree(Node* t1, Node* t2) {
+  string s1 = "";
+  string s2 = "";
+  getOrderString(t1, s1);
+  getOrderString(t2, s2);
+  return s1.find(s2) != string::npos;
+}
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  Node* t1 = make_tree();
+  Node* t2 = make_tree2();
+  Node* t3 = new Node(5);
+  cout << containesTree(t1, t2) << endl;
+  cout << containesTree(t1, t3) << endl;
+
+  return 0;
+}
