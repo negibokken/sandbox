1c0d8cf35a0bf99162456f6fae7c52a6ca34d459
negibokken <negibokken@gmail.com>
2020-04-20 01:05:58 +0900

    Add abc163 questionA

---
+++ atcoder/abc/abc163/questionA/main.cc
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int R;
+  cin >> R;
+  const double PI = 3.141592653589793238463;
+
+  cout << 2.0 * PI * R << endl;
+
+  return 0;
+}
