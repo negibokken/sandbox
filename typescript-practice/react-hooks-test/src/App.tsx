cca3efefb91c5e2413aeacd671fdf64035a36c70
negibokken <negibokken@gmail.com>
2020-04-26 15:35:00 +0900

    Add useEffect sample

--- typescript-practice/react-hooks-test/src/App.tsx
+++ typescript-practice/react-hooks-test/src/App.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppState, incrementCreator, decrementCreator } from './store/store';
 
 function App() {
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
   const count = useSelector((state: AppState) => {
     return state.count;
   });
