edf490bee0630f27bb7c0466e75018b7dfb9896b
negibokken <negibokken@gmail.com>
2020-05-10 23:53:34 +0900

    Add leetcode 219_containes_duplicate_II

---
+++ leetcode/219_containes_duplicate_II/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try true
+4 3
+1 2 3 1
+EOF
+
+## test case 2
+cat << EOF | try true
+4 1
+1 0 1 1
+EOF
+
+## test case 2
+cat << EOF | try false
+6 2
+1 2 3 1 2 3
+EOF
