0f8b587a42ae8326d83d2bdbcb45318a99d63a81
negibokken <negibokken@gmail.com>
2020-05-15 22:55:53 +0900

    Add leetcode 303_range_sum_query_immutable

---
+++ leetcode/303_range_sum_query_immutable/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 1 -1 -3
+6
+-2 0 3 -5 2 -1
+3
+0 2
+2 5
+0 5
+EOF
+
