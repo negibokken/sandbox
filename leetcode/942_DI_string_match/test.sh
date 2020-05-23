2bbe474db08a06cad7b1a170a9f40811cb7de720
negibokken <negibokken@gmail.com>
2020-05-23 17:25:43 +0900

    Add leetcode 942_DI_string_match

---
+++ leetcode/942_DI_string_match/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 0 4 1 3 2
+IIDD
+EOF
+
+## test case 2
+cat << EOF | try 0 1 2 3
+III
+EOF
+
+## test case 2
+cat << EOF | try 3 2 0 1
+DDI
+EOF
