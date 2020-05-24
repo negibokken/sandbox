f5a43ae1dff27e25111d7a76eceb6349d1b2dde4
negibokken <negibokken@gmail.com>
2020-05-24 22:39:30 +0900

    Add leetcode 1287_element_appearing_more_than_25_in_sorted_array

---
+++ leetcode/1287_element_appearing_more_than_25_in_sorted_array/test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 6
+9
+1 2 2 6 6 6 6 7 10
+EOF
+
+
+## test case 1
+cat << EOF | try 1
+1
+1
+EOF
+
