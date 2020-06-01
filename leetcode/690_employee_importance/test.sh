1cab7d60e3d8bd511d44ad4a49721e2eb3e898de
negibokken <negibokken@gmail.com>
2020-06-01 23:54:46 +0900

    Add leetcode 690_employee_importance

---
+++ leetcode/690_employee_importance/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 11
+3 1
+1 5 2 2 3
+2 3 0
+3 3 0
+EOF
+
