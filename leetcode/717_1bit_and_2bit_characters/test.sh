b388caee379f803f188f91af4faf1dc0442cbbfc
negibokken <negibokken@gmail.com>
2020-06-04 07:11:34 +0900

    Add leetcode 717_1bit_and_2bit_characters

---
+++ leetcode/717_1bit_and_2bit_characters/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try True
+3
+1 0 0
+EOF
+
+## test case 2
+cat << EOF | try False
+4
+1 1 1 0
+EOF
