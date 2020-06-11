b8d3b785e380b45a0f98fd36d238fa80d44910fd
negibokken <negibokken@gmail.com>
2020-06-11 20:56:41 +0900

    Add leetcode 21_merge_two_sorted_lists

---
+++ leetcode/21_merge_two_sorted_lists/main.cc
@@ -0,0 +1,126 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<int, char> Pc;
+typedef pair<int, string> Ps;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NULL), right(NULL) {} };
+// clang-format on
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+  ListNode* list = new ListNode(0);
+  ListNode* cur = list;
+  while (l1 != nullptr || l2 != nullptr) {
+    if (l1 == nullptr) {
+      cur->next = l2;
+      l2 = l2->next;
+    } else if (l2 == nullptr) {
+      cur->next = l1;
+      l1 = l1->next;
+    } else if (l1->val < l2->val) {
+      cur->next = l1;
+      l1 = l1->next;
+    } else {
+      cur->next = l2;
+      l2 = l2->next;
+    }
+    cur = cur->next;
+  }
+  cur = list->next;
+  delete list;
+  return cur;
+}
+
+ListNode* CinList(int n) {
+  int x;
+  ListNode *list = nullptr, *prev = nullptr;
+  REP(i, n) {
+    cin >> x;
+    ListNode* node = new ListNode(x);
+    if (list == nullptr) {
+      list = node;
+    } else {
+      prev->next = node;
+    }
+    prev = node;
+  }
+  return list;
+}
+
+void PrintNode(ListNode* list) {
+  int cnt = 0;
+  while (list != nullptr) {
+    if (cnt++) cout << " ";
+    cout << list->val;
+    list = list->next;
+  }
+}
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int n, m;
+  cin >> n >> m;
+  ListNode *l1 = nullptr, *l2 = nullptr;
+  l1 = CinList(n);
+  l2 = CinList(m);
+  PrintNode(mergeTwoLists(l1, l2));
+
+  return 0;
+}
