0791e603e5ac4939fcde497e6c7f04275ca39054
negibokken <negibokken@gmail.com>
2020-06-12 22:10:56 +0900

    Add leetcode 387_first_character_in_a_string

--- leetcode/387_first_character_in_a_string/main.cc
+++ leetcode/387_first_character_in_a_string/main.cc
@@ -55,10 +55,10 @@ struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NU
 // clang-format on
 
 int firstUniqChar(string s) {
-  map<char, int> mp;
-  for (auto c : s) mp[c]++;
+  int mp[26] = {0};
+  for (auto c : s) mp[c - 'a']++;
   for (int i = 0; i < s.length(); i++) {
-    if (mp[s[i]] == 1) return i;
+    if (mp[s[i] - 'a'] == 1) return i;
   }
   return -1;
 }
