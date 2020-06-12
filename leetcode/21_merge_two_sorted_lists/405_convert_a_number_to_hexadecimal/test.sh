7c4ad9c11086f22d4400300a9f3fef059fb0e4b5
negibokken <negibokken@gmail.com>
2020-06-12 21:55:38 +0900

    Add 21_merge_two_sorted_lists 405_convert_a_number_to_hexadecimal

---
+++ leetcode/21_merge_two_sorted_lists/405_convert_a_number_to_hexadecimal/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 1a
+26
+EOF
+
+## test case 2
+cat << EOF | try ffffffff
+-1
+EOF
+
+## test case 2
+cat << EOF | try fffffffe
+-2
+EOF
