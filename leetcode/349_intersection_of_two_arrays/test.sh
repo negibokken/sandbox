942723a13550501dae71c942571b8242155bcbd2
negibokken <negibokken@gmail.com>
2020-06-26 23:13:53 +0900

    Add leetcode 349_intersection_of_two_arrays

---
+++ leetcode/349_intersection_of_two_arrays/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 2
+4
+1 2 2 1
+2
+2 2
+EOF
+
+## test case 2
+cat << EOF | try 9 4
+3
+4 9 5
+5
+9 4 9 8 4
+EOF
