69e6f8675501472a685a8154a51fb6da00867162
negibokken <negibokken@gmail.com>
2020-07-05 17:46:54 +0900

    Add leetcode 1237_find_positive_integer_solution_for_a_given_equation

---
+++ leetcode/1237_find_positive_integer_solution_for_a_given_equation/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 2
+cat << EOF | try 1 5 5 1
+EOF
