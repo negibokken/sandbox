0b0c9bb49f1bc9e7e6a2e7b6169ea4cb4cc50df8
negibokken <negibokken@gmail.com>
2020-08-11 15:57:14 +0900

    Add algorithmer 5-12

---
+++ algorithmer/5-12/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 1.00
+1
+25 25 25 25
+EOF
+
+## test case 2
+cat << EOF | try 0.75
+2
+25 25 25 25
+EOF
