bd5ea1535c4e2c05cd467d06653cf0d03ee4a566
negibokken <negibokken@gmail.com>
2020-08-18 21:43:41 +0900

    Add leetcode 611_valid_triangle_number

---
+++ leetcode/611_valid_triangle_number/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 3
+4
+2 2 3 4
+EOF
+
+## test case 1
+cat << EOF | try 0
+4
+1 1 3 4
+EOF
+
