92ee8a187677b0b363661dc1750609744601792e
negibokken <negibokken@gmail.com>
2020-08-27 18:03:07 +0900

    Add leetcode 508_most_frequent_subtree_sum

---
+++ leetcode/508_most_frequent_subtree_sum/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try -3 2 4
+3
+5 2 -3
+EOF
+
+## test case 2
+cat << EOF | try 2
+3
+5 2 -5
+EOF
