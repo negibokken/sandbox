6fc04ed515cdf1777192d70923874fdb3ac64665
negibokken <negibokken@gmail.com>
2020-09-16 22:39:56 +0900

    Add leetcode 1584_min_cost_to_connect_all_points

---
+++ leetcode/1584_min_cost_to_connect_all_points/main.cc
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "bokken.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<int, char> Pc;
+typedef pair<int, string> Ps;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+#define arrin(arr,N) REP(i,(N)) { cin >> arr[i]; }
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NULL), right(NULL) {} };
+// clang-format on
+
+typedef tuple<int, int, int> T;
+struct comp {
+  bool operator()(const T& lhs, const T& rhs) const {
+    return get<0>(lhs) > get<0>(rhs);
+  }
+};
+
+int minCostConnectPoints(vector<vector<int>>& points) {
+  vector<bool> visited(points.size(), false);
+  priority_queue<T, vector<T>, comp> pq;
+  for (int i = 0; i < points.size(); i++) {
+    for (int j = i + 1; j < points.size(); j++) {
+      int cost =
+          abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+      pq.push({cost, i, j});
+    }
+  }
+  long long ans = 0;
+  while (!pq.empty()) {
+    tuple<int, int, int> tp = pq.top();
+    int u = get<1>(tp), v = get<2>(tp), cost = get<0>(tp);
+    if (!visited[u] || !visited[v]) {
+      ans += cost;
+      visited[u] = visited[v] = true;
+    }
+    pq.pop();
+  }
+  cout << "visited: ";
+  int cnt = 0;
+  for (auto v : visited) {
+    if (cnt++) cout << " ";
+    cout << v;
+  }
+  cout << endl;
+  return ans;
+}
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int n;
+  cin >> n;
+  vector<vector<int>> arr(n);
+
+  REP(i, n) {
+    vector<int> point(2);
+    REP(j, 2) { cin >> point[j]; }
+    arr[i] = point;
+  }
+
+  cout << minCostConnectPoints(arr) << endl;
+
+  return 0;
+}
---
+++ leetcode/1584_min_cost_to_connect_all_points/test.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 20
+5
+0 0
+2 2
+3 10
+5 2
+7 0
+EOF
+
+## test case 2
+cat << EOF | try 18
+3
+3 12
+-2 5
+-4 1
+EOF
+
+## test case 2
+cat << EOF | try 4
+4
+0 0
+1 1
+1 0
+-1 1
+EOF
+
+## test case 2
+cat << EOF | try 4000000
+2
+-1000000 -1000000
+1000000 1000000
+EOF
+
+## test case 2
+cat << EOF | try 0
+1
+0 0
+EOF
+
+## test case 2
+cat << EOF | try 53
+4
+2,-3
+-17,-8
+13,8
+-17,-15
+EOF
