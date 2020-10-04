6a44dd07a09f2766fc6bd446e5846aba0809379d
negibokken <negibokken@gmail.com>
2020-10-05 08:07:21 +0900

    Add leetcode 876_midd_of-the_linked_list

---
+++ leetcode/876_midd_of-the_linked_list/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 3
+5
+1 2 3 4 5
+EOF
+
+## test case 2
+cat << EOF | try 4
+6
+1 2 3 4 5 6
+EOF
