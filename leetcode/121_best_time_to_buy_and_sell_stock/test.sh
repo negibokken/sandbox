c05d4a7e575d7e8ea8651f637e11ef69232f9ee0
negibokken <negibokken@gmail.com>
2020-10-06 09:43:54 +0900

    Add leetcode 121_best_time_to_buy_and_sell_stock

---
+++ leetcode/121_best_time_to_buy_and_sell_stock/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 5
+6
+7 1 5 3 6 4
+EOF
+
+## test case 2
+cat << EOF | try 0
+5
+7 6 4 3 1
+EOF
