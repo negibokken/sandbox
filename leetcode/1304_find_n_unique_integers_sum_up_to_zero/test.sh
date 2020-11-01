2d5ce2737ae33473a0d03dd8abeeea704bec0ccf
negibokken <negibokken@gmail.com>
2020-11-01 21:55:07 +0900

    Add leetcode 1304_find_n_unique_integers_sum_up_to_zero

---
+++ leetcode/1304_find_n_unique_integers_sum_up_to_zero/test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try -7 -1 1 3 4
+5
+EOF
+
+## test case 2
+cat << EOF | try -1 0 1
+3
+EOF
