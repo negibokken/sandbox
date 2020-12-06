7853c1aadf16212b755a7fdd28087eb4b29e7d9c
negibokken <negibokken@gmail.com>
2020-12-06 09:17:10 +0900

    Add leetcode 1313_decompress_run_length_encoded_list

---
+++ leetcode/1313_decompress_run_length_encoded_list/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 2 4 4 4
+4
+1 2 3 4
+EOF
+
+## test case 2
+cat << EOF | try 1 3 3
+4
+1 1 2 3
+EOF
