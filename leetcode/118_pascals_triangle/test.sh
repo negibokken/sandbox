9052c09dadbbeab83f44a51f5ac01568ec03a927
negibokken <negibokken@gmail.com>
2020-12-11 23:57:04 +0900

    Add leetcode 118_pascals_triangle

---
+++ leetcode/118_pascals_triangle/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+
+## test case 1
+cat << EOF | try
+1
+EOF
+
+## test case 1
+cat << EOF | try
+2
+EOF
+
+## test case 1
+cat << EOF | try
+5
+EOF
+
