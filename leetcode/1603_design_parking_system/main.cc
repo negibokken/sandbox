29e50540030e69b22add1afc2db884a266ffd5c4
negibokken <negibokken@gmail.com>
2020-12-28 10:04:44 +0900

    Add leetcode 1603_design_parking_system

---
+++ leetcode/1603_design_parking_system/main.cc
@@ -0,0 +1,133 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "bokken.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<int, char> Pc;
+typedef pair<int, string> Ps;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+#define arrin(arr,N) REP(i,(N)) { cin >> arr[i]; }
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NULL), right(NULL) {} };
+// clang-format on
+
+class ParkingSystem {
+  int big, medium, small;
+
+ public:
+  ParkingSystem(int big, int medium, int small)
+      : big(big), medium(medium), small(small) {}
+
+  bool addCar(int carType) {
+    switch (carType) {
+      case 1:
+        if (big <= 0) return false;
+        --big;
+        return true;
+        break;
+      case 2:
+        if (medium <= 0) return false;
+        --medium;
+        return true;
+        break;
+      case 3:
+        if (small <= 0) return false;
+        --small;
+        return true;
+        break;
+      default:;
+    }
+    return false;
+  }
+};
+
+int getType(string s) {
+  if (s == "ParkingSystem") {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int n;
+  cin >> n;
+
+  ParkingSystem* p;
+
+  string s;
+  int t;
+  bool ans;
+  for (int i = 0; i < n; i++) {
+    if (i) cout << " ";
+    cin >> s;
+    switch (getType(s)) {
+      case 0:
+        int b, m, s;
+        cin >> b >> m >> s;
+        p = new ParkingSystem(b, m, s);
+        cout << "null";
+        break;
+      case 1:
+        cin >> t;
+        ans = p->addCar(t);
+        cout << (ans ? "true" : "false");
+        break;
+      default:;
+    }
+  }
+  cout << endl;
+
+  return 0;
+}
---
+++ leetcode/1603_design_parking_system/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try null true true false false
+5
+ParkingSystem
+1  1  0
+addCar
+1
+addCar
+2
+addCar
+3
+addCar
+1
+EOF
