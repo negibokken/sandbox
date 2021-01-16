f877e50c5cc67d0b4e60efe2038174d52f83d07f
negibokken <negibokken@gmail.com>
2021-01-16 23:46:47 +0900

    Add leetcode 748_shortest_completing_word

---
+++ leetcode/748_shortest_completing_word/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try steps
+1s3 PSt
+4
+step steps stripe stepple
+EOF
+
