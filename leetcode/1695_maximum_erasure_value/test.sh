cfa495e9ae67d6085e278f327d7bc674b051b6bc
negibokken <negibokken@gmail.com>
2021-02-27 23:37:30 +0900

    Add leetcode 1695_maximum_erasure_value

---
+++ leetcode/1695_maximum_erasure_value/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 17
+5
+4 2 4 5 6
+EOF
+
+## test case 2
+cat << EOF | try 8
+9
+5 2 1 2 5 2 1 2 5
+EOF
