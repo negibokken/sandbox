56230a9fbad3d15f25269251cc2098f601e06316
negibokken <negibokken@gmail.com>
2021-03-17 23:04:30 +0900

    Add leetcode 346_moving_average_fromd_data_stream

---
+++ leetcode/346_moving_average_fromd_data_stream/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try null 1.0 5.5 4.66667 6.0
+5
+MovingAverage next next next next
+3 1 10 3 5
+EOF
+
