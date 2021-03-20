180023968fd7e54c6a146e24b0f7aa965b4a3be4
negibokken <negibokken@gmail.com>
2021-03-20 16:44:59 +0900

    Add leetcode 419_battleships_in_a_board

---
+++ leetcode/419_battleships_in_a_board/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 2
+4 4
+X..X
+...X
+...X
+EOF
+
+## test case 1
+cat << EOF | try 2
+4 4
+X..X
+...X
+...X
+EOF
+
