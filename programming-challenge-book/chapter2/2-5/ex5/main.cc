3fee51ad9a7ee907511bcd06855627412a4b11ed
negibokken <negibokken@gmail.com>
2021-03-29 09:36:05 +0900

    Add 2-5 ex5

---
+++ programming-challenge-book/chapter2/2-5/ex5/main.cc
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "bokken.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<int, char> Pc;
+typedef pair<int, string> Ps;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+#define arrin(arr,N) REP(i,(N)) { cin >> arr[i]; }
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NULL), right(NULL) {} };
+// clang-format on
+
+struct edge {
+  int to, cost;
+};
+int V;
+int d[MAX_N];
+vector<edge> G[MAX_N];
+int prevv[MAX_N];
+
+void dijkstra(int s) {
+  priority_queue<P, vector<P>, greater<P>> que;
+  fill(d, d + V, inf);
+  fill(prevv, prevv + V, -1);
+  d[s] = 0;
+  que.push(P(0, s));
+  while (!que.empty()) {
+    P p = que.top();
+    que.pop();
+    int v = p.second;
+    if (d[v] < p.first) continue;
+    for (int i = 0; i < G[v].size(); i++) {
+      edge e = G[v][i];
+      if (d[e.to] > d[v] + e.cost) {
+        d[e.to] = d[v] + e.cost;
+        que.push(P(d[e.to], e.to));
+        prevv[e.to] = v;
+      }
+    }
+  }
+}
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  int E;
+  cin >> V >> E;
+  int u, v, c;
+  REP(i, E) {
+    cin >> u >> v >> c;
+    G[u].push_back(edge{v, c});
+    G[v].push_back(edge{u, c});
+  }
+  dijkstra(0);
+
+  int t = 6;
+  vector<int> path;
+  for (; t != -1; t = prevv[t]) path.push_back(t);
+  reverse(path.begin(), path.end());
+  for (int i = 0; i < path.size(); i++) {
+    if (i) cout << " ";
+    cout << path[i];
+  }
+  cout << endl;
+
+  return 0;
+}
