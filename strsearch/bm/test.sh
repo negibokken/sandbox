0bf925a6cc910eeae4c038cfaf80cdc0fae73943
negibokken <negibokken@gmail.com>
2021-03-31 07:46:25 +0900

    Add strsearch bm

---
+++ strsearch/bm/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 3 -1 0 3
+4
+deff
+abcdeffg
+abcdefg
+deff
+effdeff
+EOF
+
