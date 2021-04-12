001b90071c3933bffc9e9a6303ae4d252d7d4cc9
negibokken <negibokken@gmail.com>
2021-04-13 00:24:42 +0900

    Add 3-4 ex1

---
+++ programming-challenge-book/chapter3/3-4/ex1/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 22
+5 8
+0 3 4
+0 1 3
+1 2 5
+2 3 5
+2 0 4
+3 4 3
+4 0 7
+4 1 6
+EOF
+
