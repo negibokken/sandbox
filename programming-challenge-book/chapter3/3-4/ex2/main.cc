97973a07322b6a76493b1e846e4e347a189a2502
negibokken <negibokken@gmail.com>
2021-04-16 22:14:47 +0900

    Add 3-4 ex2

---
+++ programming-challenge-book/chapter3/3-4/ex2/main.cc
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "bokken.h"
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> P;
+typedef pair<int, char> Pc;
+typedef pair<int, string> Ps;
+
+const int dx[4] = {-1, 0, 0, 1};
+const int dy[4] = {0, -1, 1, 0};
+const int dx8[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int dy8[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const string dir8[8] = {"U", "RU", "R", "RD", "D", "LD", "L", "LU"};
+
+// Self settings
+// clang-format off
+#define MAX_N 100000
+#define MAX_M 30
+#define MAX 100000
+#define INFTY (1<<30)
+#define EPS (1e-10)
+#define equals(a, b) (fabs((a) - (b)) < EPS)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i, N) for (int i = 0; i < (int)(N); ++i)
+#define SLN(i,N) (i == N-1 ? "\n" : " ")
+#define arrin(arr,N) REP(i,(N)) { cin >> arr[i]; }
+ll fact(ll n) { ll res = 1; for(ll i=2;i<=n;++i) res = res * i; return res;}
+ll nCr(ll n, ll r) {return (fact(n)/fact(n-r)*fact(r)) ;}
+ll gcd(ll a,ll b){if(b==0)return a;return gcd(b,a%b);}
+ll lcm(ll a,ll b){return a/gcd(a,b)*b;}
+const ll MOD = 1e9+7;
+const ll INF = 1LL << 60;
+const int inf = 1000100011;
+class Point { public: double x, y; Point(double x = 0, double y = 0) : x(x), y(y) {} Point operator+(Point p) { return Point(x + p.x, y + p.y); } Point operator-(Point p) { return Point(x - p.x, y - p.y); } Point operator*(double a) { return Point(a * x, a * y); } Point operator/(double a) { return Point(x / a, y / a); } double abs() { return sqrt(norm()); } double norm() { return x * x + y * y; } bool operator<(const Point& p) const { return x != p.x ? x < p.x : y < p.y; } bool operator==(const Point& p) const { return fabs(x - p.y) < EPS && fabs(y - p.y) < EPS; } };
+typedef Point Vector;
+double norm(Vector a) { return a.x * a.x + a.y * a.y; }
+double abs(Vector a) { return sqrt(norm(a));}
+double dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
+double cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
+struct Segment { Point p1, p2; };
+typedef Segment Line;
+struct Node { int data; Node *left, *right; Node(int data) : data(data), left(NULL), right(NULL) {} };
+// clang-format on
+
+int n, m, a, b;
+int t[MAX_N];
+int d[MAX_M][MAX_M];
+
+double dp[1 << MAX_M][MAX_M];
+
+void solve() {
+  for (int i = 0; i <= 1 << n; i++) {
+    fill(dp[i], dp[i] + m, inf);
+  }
+  dp[(1 << n) - 1][a - 1] = 0;
+  double res = inf;
+  for (int S = (1 << n) - 1; S >= 0; S--) {
+    res = min(res, dp[S][b - 1]);
+    for (int v = 0; v < m; v++) {
+      for (int i = 0; i < n; i++) {
+        if (S >> i & 1) {
+          for (int u = 0; u < m; u++) {
+            if (d[v][u] >= 0) {
+              dp[S & ~(1 << i)][u] =
+                  min(dp[S & ~(1 << i)][u], dp[S][v] + (double)d[v][u] / t[i]);
+            }
+          }
+        }
+      }
+    }
+  }
+  if (res == inf) {
+    printf("Impossible\n");
+  } else {
+    printf("%.3f\n", res);
+  }
+}
+
+int main(void) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(15);
+  cin >> n >> m >> a >> b;
+  REP(i, n) cin >> t[i];
+  solve();
+
+  return 0;
+}
---
+++ programming-challenge-book/chapter3/3-4/ex2/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  #actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 3.667
+2 4 2 1
+3 1
+EOF
+
