74c9f9722a0ad5e72b3ac4b2dc97d473abee88f2
negibokken <negibokken@gmail.com>
2021-04-17 21:08:20 +0900

    Add leetcode 695_max_area_of_islnad

---
+++ leetcode/695_max_area_of_islnad/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  #actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 6
+8 13
+0 0 1 0 0 0 0 1 0 0 0 0 0
+0 0 0 0 0 0 0 1 1 1 0 0 0
+0 1 1 0 1 0 0 0 0 0 0 0 0
+0 1 0 0 1 1 0 0 1 0 1 0 0
+0 1 0 0 1 1 0 0 1 1 1 0 0
+0 0 0 0 0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 0 1 1 1 0 0 0
+0 0 0 0 0 0 0 1 1 0 0 0 0
+EOF
+
+## test case 2
+cat << EOF | try 0
+1 8
+0 0 0 0 0 0 0 0
+EOF
+
+cat << EOF | try 4
+4 5
+1 1 0 0 0
+1 1 0 0 0
+0 0 0 1 1
+0 0 0 1 1
+EOF
