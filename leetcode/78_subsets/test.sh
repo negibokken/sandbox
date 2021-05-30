a61731b4b0bd5b11bc3f043c32440088475f25b7
bokken <negibokken@gmail.com>
2021-05-30 17:09:41 +0900

    Add leetcode 78_subsets

---
+++ leetcode/78_subsets/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try "[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]"
+3
+1 2 3
+EOF
+
+## test case 2
+cat << EOF | try "[[],[0]]"
+1
+0
+EOF
