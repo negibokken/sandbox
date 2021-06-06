7ec8bbeda65e40812368ac075fb3eb6e64cd3b18
bokken <negibokken@gmail.com>
2021-06-06 18:34:12 +0900

    Add leetcode 894_all_possible_full_binary_trees

---
+++ leetcode/894_all_possible_full_binary_trees/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  #actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try "[[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]"
+7
+EOF
+
+## test case 2
+cat << EOF | try "[[0,0,0]]"
+3
+EOF
