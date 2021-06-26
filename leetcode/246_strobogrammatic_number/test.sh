6e5a54145613fe9f771a34ed17bf1886791bcd63
bokken <negibokken@gmail.com>
2021-06-26 14:41:43 +0900

    Add leetcode 246_strobogrammatic_number

---
+++ leetcode/246_strobogrammatic_number/test.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try true
+69
+EOF
+
+## test case 2
+cat << EOF | try true
+88
+EOF
+
+## test case 2
+cat << EOF | try false
+962
+EOF
+
+## test case 2
+cat << EOF | try true
+1
+EOF
