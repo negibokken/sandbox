44310fd25f2e9bcaf83c61fbad287a8e1f1cb7a6
bokken <negibokken@gmail.com>
2021-07-25 18:16:36 +0900

    Add leetcode 57_insert_interval

---
+++ leetcode/57_insert_interval/main.py
@@ -0,0 +1,6 @@
+#/usr/bin/python3
+
+from typing import List
+
+
+sol = Solution()
---
+++ leetcode/57_insert_interval/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | python3 ./main.py`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 1 3 3 3 4 5 6
+7
+6 5 3 1 3 4 3
+EOF
+
+## test case 2
+cat << EOF | try 2 3 3 4
+4
+3 4 3 2
+EOF
