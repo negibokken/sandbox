021e3baad0d52409563badef0f12dd61eb05dc41
bokken <negibokken@gmail.com>
2021-08-23 00:48:46 +0900

    Add leetcode 922_sort_array_by_parity_II

--- leetcode/922_sort_array_by_parity_II/main.py
+++ leetcode/922_sort_array_by_parity_II/main.py
@@ -6,19 +6,23 @@ import json
 
 class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if (i % 2 == 0) and nums[i] != 0:
-                j = i+1
-                while j < len(nums) and (nums[j] % 2) == 0:
-                    j += 2
-                if (j % 2 != 0) and nums[j] == 0:
-                    nums[i], nums[j] = nums[j], nums[i]
-            elif (i % 2 == 1) and (nums[i] % 2) != 1:
-                j = i + 1
-                while j < len(nums) and (nums[j] % 2) == 0:
-                    j += 2
-            nums[i], nums[j] = nums[j], nums[i]
-        return nums
+        N = len(nums)
+        ans = [0] * N
+
+        t = 0
+        for x in nums:
+            if x % 2 == 0:
+                ans[t] = x
+                t += 2
+        t = 1
+        for x in nums:
+            if x % 2 == 1:
+                ans[t] = x
+                t += 2
+        return ans
+
+
+
 
 
 arr = json.loads(input())
