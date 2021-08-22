f7368f029311008e094fdfb6a628d774963c5111
bokken <negibokken@gmail.com>
2021-08-22 23:03:16 +0900

    Add leetcode 922_sort_array_by_parity_II

--- leetcode/922_sort_array_by_parity_II/main.py
+++ leetcode/922_sort_array_by_parity_II/main.py
@@ -8,13 +8,16 @@ class Solution:
     def sortArrayByParityII(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
             if (i % 2 == 0) and nums[i] != 0:
-                for j in range(len(nums)):
-                    if (j % 2 != 0) and nums[j] == 0:
-                        nums[i], nums[j] = nums[j], nums[i]
+                j = i+1
+                while j < len(nums) and (nums[j] % 2) == 0:
+                    j += 2
+                if (j % 2 != 0) and nums[j] == 0:
+                    nums[i], nums[j] = nums[j], nums[i]
             elif (i % 2 == 1) and (nums[i] % 2) != 1:
-                for j in range(len(nums)):
-                    if (j % 2 != 1) and (nums[j] % 2) == 1:
-                        nums[i], nums[j] = nums[j], nums[i]
+                j = i + 1
+                while j < len(nums) and (nums[j] % 2) == 0:
+                    j += 2
+            nums[i], nums[j] = nums[j], nums[i]
         return nums
 
 
