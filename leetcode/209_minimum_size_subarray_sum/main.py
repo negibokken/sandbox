1a3b37badc3a1b4debca36bf68c4df83d52ec7d4
bokken <negibokken@gmail.com>
2021-08-29 21:48:50 +0900

    Add leetcode 209_minimum_size_subarray_sum

--- leetcode/209_minimum_size_subarray_sum/main.py
+++ leetcode/209_minimum_size_subarray_sum/main.py
@@ -8,21 +8,19 @@ import sys
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         ans = sys.maxsize
-        sum = 0
-        i, j = 0, 0
+        i, j, sum = 0, 0, 0
         while i < len(nums):
-            while sum >= target:
-                sum += nums[i]
-                i += 1
-            j = i
             while sum < target and j < len(nums):
-                sum -= nums[j]
+                sum += nums[j]
                 j += 1
-            if sum >= target:
-                print(sum, i, j)
+            if sum < target:
+                i += 1
+                continue
+            while sum >= target:
                 ans = min(ans, j - i)
-        return 0 if ans == sys.maxsize else ans
-
+                sum -= nums[i]
+                i += 1
+        return ans if ans is not sys.maxsize else 0
 
 target = int(input())
 arr = json.loads(input())
