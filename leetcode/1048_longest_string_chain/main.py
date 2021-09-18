d38d558448545b350f5fa45b2b42387e2d41aa91
bokken <negibokken@gmail.com>
2021-09-18 14:32:19 +0900

    Add leetcode 1048_longest_string_chain

---
+++ leetcode/1048_longest_string_chain/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+
+
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        d = dict()
+        for word in words:
+            d[word] = 1
+        longest = 1
+        for word in sorted(words, key=len):
+            for i in range(len(word)):
+                prev = word[:i] + word[i+1:]
+                if prev in d:
+                    d[word] = max(d[word], d[prev] + 1)
+            longest = max(longest, d[word])
+        return longest
+
+
+arr = json.loads(input())
+
+sol = Solution()
+print(sol.longestStrChain(arr))
