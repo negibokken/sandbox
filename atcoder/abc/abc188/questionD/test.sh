8476a75803d2f6ab6b0fe968d3f7c8f897a5583b
bokken <negibokken@gmail.com>
2021-09-28 00:05:00 +0900

    Add abc188 questionD

---
+++ atcoder/abc/abc188/questionD/test.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | ./main`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 10
+2 6
+1 2 4
+2 2 4
+EOF
+
+## test case 2
+cat << EOF | try 163089627821228
+5 1000000000
+583563238 820642330 44577
+136809000 653199778 90962
+54601291 785892285 50554
+5797762 453599267 65697
+468677897 916692569 87409
+EOF
+
+## test case 2
+cat << EOF | try 88206004785464
+5 100000
+583563238 820642330 44577
+136809000 653199778 90962
+54601291 785892285 50554
+5797762 453599267 65697
+468677897 916692569 87409
+EOF
