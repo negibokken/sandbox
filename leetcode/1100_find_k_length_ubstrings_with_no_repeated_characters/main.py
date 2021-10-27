0a0df494d07cfdd64e415927e931870247b5f09a
bokken <negibokken@gmail.com>
2021-10-27 19:05:26 +0900

    Add leetcode 1100_find_k_length_ubstrings_with_no_repeated_characters

---
+++ leetcode/1100_find_k_length_ubstrings_with_no_repeated_characters/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import json
+
+
+class Solution:
+    def numKLenSubstrNoRepeats(self, s: str, k: int) -> int:
+        ans = 0
+        for i in range(len(s)):
+            f = True
+            mp = {}
+            for j in range(i, min(i + k, len(s))):
+                if s[j] in mp:
+                    f = False
+                    break
+                mp[s[j]] = True
+            if f:
+                ans += 1
+        return ans
+
+
+s = input().rstrip("\n")
+k = int(input())
+
+sol = Solution()
+print(sol.numKLenSubstrNoRepeats(s, k))
