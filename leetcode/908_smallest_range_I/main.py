5731624ba09dc951f8ebb8901c39de773056c110
bokken <negibokken@gmail.com>
2021-10-31 08:54:43 +0900

    Add leetcode 908_smallest_range_I

---
+++ leetcode/908_smallest_range_I/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+from bplib.butil import TreeNode, arr2TreeNode, btreeconnect
+
+
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        return 11
+
+
+arr = json.loads(input())
+k = int(input())
+print(arr, k)
+
+sol = Solution()
+print(sol.smallestRangeI(arr, k))
---
+++ leetcode/908_smallest_range_I/test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | python3 ./main.py`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 0
+[1]
+0
+EOF
+
+## test case 2
+cat << EOF | try 6
+[0,10]
+2
+EOF
+
+## test case 3
+cat << EOF | try 0
+[1,3,6]
+3
+EOF
