8f3f964889830a7bff7f6c1d4911315a7580c6b0
bokken <negibokken@gmail.com>
2021-11-06 00:03:52 +0900

    Add leetcode 530_minimum_absolute_difference_in_bst

--- leetcode/530_minimum_absolute_difference_in_bst/main.py
+++ leetcode/530_minimum_absolute_difference_in_bst/main.py
@@ -6,12 +6,6 @@ from bplib.butil import TreeNode, arr2TreeNode, btreeconnect
 import sys
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
 
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
