f6b6c77bacd407fd4ac67f185696f4c19f5a15b7
bokken <negibokken@gmail.com>
2021-11-24 13:21:11 +0900

    Add leetcode 1929_concatenation_of_array

--- leetcode/1929_concatenation_of_array/main.py
+++ leetcode/1929_concatenation_of_array/main.py
@@ -7,9 +7,9 @@ from bplib.butil import aprint
 
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-        ans.extend(nums)
-        ans.extend(nums)
+        ans = [0] * (2*len(nums))
+        for i in range(2*len(nums)):
+            ans[i] = nums[(i % len(nums))]
         return ans
 
 
