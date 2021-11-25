c2ee42b1d7668c0ed3b672a8f961936c236ae3fe
bokken <negibokken@gmail.com>
2021-11-25 16:08:34 +0900

    Add leetcode 69_sqrt_x

---
+++ leetcode/69_sqrt_x/test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+try() {
+  expected="$@"
+  input=`cat -`
+
+  actual=`echo "$input" | python3 ./main.py`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    # echo "$input => $actual"
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+## test case 1
+cat << EOF | try 2
+4
+EOF
+
+## test case 2
+cat << EOF | try 2
+8
+EOF
+
+## test case 2
+cat << EOF | try 1
+2
+EOF
+
+## test case 2
+cat << EOF | try 1
+3
+EOF
+
+## test case 2
+cat << EOF | try 2
+6
+EOF
+
+## test case 2
+cat << EOF | try 3
+13
+EOF
+
+
+cat << EOF | try 4
+16
+EOF
+
+cat << EOF | try 4
+17
+EOF
