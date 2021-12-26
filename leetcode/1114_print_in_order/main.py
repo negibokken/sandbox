b689b406bda6789d507be9752f930e8ac84fb0c5
bokken <negibokken@gmail.com>
2021-12-27 08:53:11 +0900

    Add leetcode 1114_print_in_order

--- leetcode/1114_print_in_order/main.py
+++ leetcode/1114_print_in_order/main.py
@@ -40,7 +40,7 @@ class Foo:
             # printSecond() outputs "second".
             printSecond()
             # Notify the thread that is waiting for the second job to be done.
-            Foo.secondJobDone.release()
+        Foo.secondJobDone.release()
 
     def third(self, printThird: 'Callable[[], None]') -> None:
 
@@ -60,10 +60,10 @@ with futures.ThreadPoolExecutor(max_workers=4) as executor:
     for n, i in enumerate(arr):
         f = None
         if i == 1:
-            f = executor.submit(fn=sol.first(printFirst), index=n)
+            f = executor.submit(sol.first, printFirst)
         elif i == 2:
-            f = executor.submit(fn=sol.second(printSecond), index=n)
+            f = executor.submit(sol.second, printSecond)
         else:
-            f = executor.submit(fn=sol.third(printThird), index=n)
+            f = executor.submit(sol.third, printThird)
         future_list.append(f)
     _ = futures.as_completed(fs=future_list)
