f8208e751976e77bdca7804c861c2d2c49890f8c
bokken <negibokken@gmail.com>
2021-12-27 08:45:53 +0900

    Add leetcode 1114_print_in_order

--- leetcode/1114_print_in_order/main.py
+++ leetcode/1114_print_in_order/main.py
@@ -2,6 +2,7 @@
 
 from typing import Callable
 import json
+from threading import Lock
 from concurrent import futures
 
 
@@ -18,36 +19,38 @@ def printThird():
 
 
 class Foo:
-    called = [False] * 3
-
     def __init__(self):
-        pass
-
-    def wait(self, num):
-        while not Foo.called[num-1]:
-            pass
+        self.firstJobDone = Lock()
+        self.secondJobDone = Lock()
+        self.firstJobDone.acquire()
+        self.secondJobDone.acquire()
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
-        # printFirst() outputs "first". Do not change or remove this line.
+        # printFirst() outputs "first".
         printFirst()
-        Foo.called[0] = True
+        # Notify the thread that is waiting for the first job to be done.
+        self.firstJobDone.release()
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
-        self.wait(1)
-        # printSecond() outputs "second". Do not change or remove this line.
-        printSecond()
-        Foo.called[1] = True
+        # Wait for the first job to be done
+        with self.firstJobDone:
+            # printSecond() outputs "second".
+            printSecond()
+            # Notify the thread that is waiting for the second job to be done.
+            self.secondJobDone.release()
 
     def third(self, printThird: 'Callable[[], None]') -> None:
-        self.wait(2)
-        # printThird() outputs "third". Do not change or remove this line.
-        printThird()
-        Foo.called[2] = True
+
+        # Wait for the second job to be done.
+        with self.secondJobDone:
+            # printThird() outputs "third".
+            printThird()
 
 
 arr = json.loads(input())
 
 sol = Foo()
+
 
 future_list = []
 with futures.ThreadPoolExecutor(max_workers=4) as executor:
