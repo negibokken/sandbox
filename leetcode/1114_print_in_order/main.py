971f04218b9499b65bef73e8ad3d857fe1a3acf9
bokken <negibokken@gmail.com>
2021-12-27 08:46:48 +0900

    Add leetcode 1114_print_in_order

--- leetcode/1114_print_in_order/main.py
+++ leetcode/1114_print_in_order/main.py
@@ -19,30 +19,33 @@ def printThird():
 
 
 class Foo:
+    firstJobDone = Lock()
+    secondJobDone = Lock()
+
     def __init__(self):
-        self.firstJobDone = Lock()
-        self.secondJobDone = Lock()
-        self.firstJobDone.acquire()
-        self.secondJobDone.acquire()
+        Foo.firstJobDone = Lock()
+        Foo.secondJobDone = Lock()
+        Foo.firstJobDone.acquire()
+        Foo.secondJobDone.acquire()
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
         # printFirst() outputs "first".
         printFirst()
         # Notify the thread that is waiting for the first job to be done.
-        self.firstJobDone.release()
+        Foo.firstJobDone.release()
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
         # Wait for the first job to be done
-        with self.firstJobDone:
+        with Foo.firstJobDone:
             # printSecond() outputs "second".
             printSecond()
             # Notify the thread that is waiting for the second job to be done.
-            self.secondJobDone.release()
+            Foo.secondJobDone.release()
 
     def third(self, printThird: 'Callable[[], None]') -> None:
 
         # Wait for the second job to be done.
-        with self.secondJobDone:
+        with Foo.secondJobDone:
             # printThird() outputs "third".
             printThird()
 
