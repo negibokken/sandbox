70b2583b48e972431903f201798c69bf0b22cdc4
bokken <negibokken@gmail.com>
2022-01-03 23:53:33 +0900

    Add leetcode 997_find_the_town_judge

---
+++ leetcode/997_find_the_town_judge/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        return 1
+
+
+n = int(input())
+trust = json.loads(input())
+
+sol = Solution()
+print(sol.findJudge(n, trust))
