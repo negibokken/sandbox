ba87ff028a5b6df863b8ca5e8ee21133a1f26381
bokken <negibokken@gmail.com>
2022-01-15 00:00:06 +0900

    Add leetcode 117_populating_next_right_pointers_in_each_node_II

---
+++ leetcode/117_populating_next_right_pointers_in_each_node_II/main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+from bplib.butil import TreeNode, arr2TreeNode, btreeconnect
+from collections import deque
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return
+        q = deque()
+        while q:
+            node = q.pop()
+            if not node:
+                continue
+            nextlevel = deque()
+            q.append(node.left)
+            q.append(node.right)
+
+
+arr = json.loads(input())
+nodes = list(map(arr2TreeNode, arr))
+root = btreeconnect(nodes)
+
+sol = Solution()
+ans = sol.connect(root)
+
+
+def visit(node):
+    ans = []
+    q = deque()
+    q.append(node)
+    while q:
+        node = q.pop()
