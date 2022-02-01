742e9728987651d1e5921f41bb61291112a459c1
bokken <negibokken@gmail.com>
2022-02-01 22:04:07 +0900

    Add leetcode 1002_find_common_characters

--- leetcode/1002_find_common_characters/main.py
+++ leetcode/1002_find_common_characters/main.py
@@ -3,33 +3,16 @@
 from typing import List
 import json
 from bplib.butil import TreeNode, arr2TreeNode, btreeconnect
+import collections
 
 
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        if len(words) == 0:
-            return []
-        dicts = [{} for i in range(len(words))]
-        for i, word in enumerate(words):
-            for c in word:
-                if c not in dicts[i]:
-                    dicts[i][c] = 0
-                dicts[i][c] += 1
-        ans = []
-        for c, num in dicts[0].items():
-            minVal = num
-            isAllContains = True
-            for i in range(1, len(words)):
-                if c not in dicts[i]:
-                    isAllContains = False
-                    break
-                else:
-                    minVal = min(minVal, dicts[i][c])
-            if not isAllContains:
-                continue
-            for i in range(minVal):
-                ans.append(c)
-        return ans
+        res = collections.Counter(words[0])
+        for a in words:
+            res &= collections.Counter(a)
+            print(res)
+        return list(res.elements())
 
 
 arr = json.loads(input())
