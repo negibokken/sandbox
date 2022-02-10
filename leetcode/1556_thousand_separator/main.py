99a835138c2fdd6f4f1ebf55f52bc4c1e17f1576
bokken <negibokken@gmail.com>
2022-02-11 08:50:33 +0900

    Add leetcode 1556_thousand_separator

---
+++ leetcode/1556_thousand_separator/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+from bplib.butil import TreeNode, arr2TreeNode, btreeconnect
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        return 1
+
+
+arr = json.loads(input())
+nodes = list(map(arr2TreeNode, arr))
+root = btreeconnect(nodes)
+
+sol = Solution()
