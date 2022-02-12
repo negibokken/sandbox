efe902ba77a131a36bf8e9e3d147c9dbd0b84c44
bokken <negibokken@gmail.com>
2022-02-12 11:48:13 +0900

    Add leetcode 1556_thousand_separator

--- leetcode/1556_thousand_separator/main.py
+++ leetcode/1556_thousand_separator/main.py
@@ -7,14 +7,15 @@ from bplib.butil import TreeNode, arr2TreeNode, btreeconnect
 
 class Solution:
     def thousandSeparator(self, n: int) -> str:
+        if not n:
+            return "0"
         i = 0
         s = ""
         while n > 0:
-            print(n % 10)
-            s = chr(n % 10) + s
+            s = str(n % 10) + s
             n //= 10
             i += 1
-            if i % 2 == 0 and n != 0:
+            if i % 3 == 0 and n != 0:
                 s = '.' + s
 
         return s
