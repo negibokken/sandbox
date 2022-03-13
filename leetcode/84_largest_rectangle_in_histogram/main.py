4ebe454c1373ec5531f9438cde462a02361009fb
Ryo Ogawa <negibokken@gmail.com>
2022-03-13 22:20:18 +0900

    Add leetcode 84_largest_rectangle_in_histogram

---
+++ leetcode/84_largest_rectangle_in_histogram/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+from bplib.butil import TreeNode, arr2TreeNode, btreeconnect, aprint
+
+
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        stack = [-1]
+        max_area = 0
+        for i in range(len(heights)):
+            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+                current_height = heights[stack.pop()]
+                current_width = i - stack[-1] - 1
+                max_area = max(max_area, current_height * current_width)
+            stack.append(i)
+
+        while stack[-1] != -1:
+            current_height = heights[stack.pop()]
+            current_width = len(heights) - stack[-1] - 1
+            max_area = max(max_area, current_height * current_width)
+        return max_area
+
+
+arr = json.loads(input())
+
+sol = Solution()
+print(sol.largestRectangleArea(arr))
---
+++ leetcode/84_largest_rectangle_in_histogram/test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+try() {
+  file=$1
+  expected="${@:2:($#-1)}"
+  input=`cat -`
+
+  actual=`echo "$input" | python3 ./$file`
+
+  actual=`echo "$actual"`
+  actual=`echo $actual | sed 's/\n/ /g'`
+  expected=`echo "$expected"`
+
+  if [ "$actual" = "$expected" ]; then
+    echo "OK"
+  else
+    echo "==="
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+for file in `ls *.py`; do
+echo $file "==="
+
+## test case 1
+cat << EOF | try $file 10
+[2,1,5,6,2,3]
+EOF
+
+## test case 2
+cat << EOF | try $file 4
+[2,4]
+EOF
+
+done
