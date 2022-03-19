5919a80177c08af1665bde562611f5678c7e9cf4
Ryo Ogawa <negibokken@gmail.com>
2022-03-19 19:58:40 +0900

    Add leetcode 101_symmetric_tree

--- leetcode/101_symmetric_tree/main.py
+++ leetcode/101_symmetric_tree/main.py
@@ -18,20 +18,14 @@ def prin(a):
 
 class Solution:
     def isSame(self, left, right):
-        print(left, right)
         if (not right and left) or (right and not left):
-            print("here: Falsse")
             return False
         elif not right and not left:
-            print("here: True")
             return True
         else:
             if left.val != right.val:
-                print("False: ", left.val, right.val)
                 return False
-            print(left.val, right.val)
-            print(">>")
-            return self.isSame(left.left, right.left) and self.isSame(left.right, right.right)
+            return self.isSame(left.left, right.right) and self.isSame(left.right, right.left)
 
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if not root:
@@ -44,17 +38,4 @@ nodes = list(map(arr2TreeNode, arr))
 root = btreeconnect(nodes)
 
 sol = Solution()
-
-
-def bfs(node):
-    if not node:
-        return
-    print(node.val)
-    bfs(node.left)
-    bfs(node.right)
-
-
-bfs(root)
-
-
-# print(sol.isSymmetric(root))
+print(sol.isSymmetric(root))
