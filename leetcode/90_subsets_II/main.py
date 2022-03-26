b9efc47a1275275f91757c8a68beda90e574757f
Ryo Ogawa <negibokken@gmail.com>
2022-03-26 11:52:06 +0900

    Add leetcode 90_subsets_II

--- leetcode/90_subsets_II/main.py
+++ leetcode/90_subsets_II/main.py
@@ -7,7 +7,23 @@ from bplib.butil import TreeNode, arr2TreeNode, btreeconnect, aprint
 
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        return [[]]
+        ans = []
+        n = len(nums)
+        nums = sorted(nums)
+        st = set()
+
+        def backtrack(first, arr):
+            f = "{}".format(arr)
+            if not f in st:
+                ans.append(arr)
+                st.add(f)
+            for i in range(first, n):
+                arr.append(nums[i])
+                backtrack(i+1, arr[:])
+                arr.pop()
+        backtrack(0, [])
+
+        return ans
 
 
 arr = json.loads(input())
