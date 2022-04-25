3dc3405b3a093d55f8ee7033883e3c6b7f222a5a
Ryo Ogawa <negibokken@gmail.com>
2022-04-26 06:55:31 +0900

    Add leetcode 293_flip_game

---
+++ leetcode/293_flip_game/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from typing import List
+import json
+from bplib.butil import TreeNode, arr2TreeNode, btreeconnect, aprint
+
+
+class Solution:
+    def generatePossibleNextMoves(self, currentState: str) -> List[str]:
+        return ["aaaa"]
+
+
+arr = input()
+
+sol = Solution()
+print(sol.generatePossibleNextMoves(arr))
