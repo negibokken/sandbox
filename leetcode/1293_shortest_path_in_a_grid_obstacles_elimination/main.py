c1a024c1a8f2ddce670e66bc179d90ce2b5fb42e
Ryo Ogawa <negibokken@gmail.com>
2022-06-12 17:54:28 +0900

    Add leetcode 1293_shortest_path_in_a_grid_obstacles_elimination

--- leetcode/1293_shortest_path_in_a_grid_obstacles_elimination/main.py
+++ leetcode/1293_shortest_path_in_a_grid_obstacles_elimination/main.py
@@ -3,51 +3,31 @@
 from typing import List
 import json
 from bplib.butil import TreeNode, arr2TreeNode, btreeconnect, aprint
-import sys
-
-
-dir = [
-    [0, -1],  # up
-    [1, 0],  # right
-    [0, 1],  # down
-    [-1, 0],  # left
-]
+import collections
 
 
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        inf = sys.maxsize
-        dp = [[[inf] * len(grid[0]) for i in range(len(grid))]
-              for _ in range(k+1)]
-        dp[k][0][0] = 0
-        curx, cury = 0, 0
-        m, n = len(grid[0]), len(grid)
+        if len(grid) == 1 and len(grid[0]) == 1:
+            return 0
+        q = collections.deque([(0, 0, 0, 0)])
+        m, n = len(grid), len(grid[0])
+        visited = set()
 
-        def bfs(curx, cury):
-            for nextx, nexty in ((curx+1, cury), (curx, cury+1), (curx-1, cury), (curx, cury-1)):
-                if not (0 <= nextx < m) or not (0 <= nexty < n):
+        while q:
+            x, y, obstacle, path = q.popleft()
+            for i, j in ((x+1, y), (x, y+1), (x-1, y), (x, y-1)):
+                if not (0 <= i < m and 0 <= j < n):
                     continue
-                offset = 0
-
-                if grid[nexty][nextx] == 1:
-                    offset = 1
-                updated = False
-                for i in range(k, -1, -1):
-                    if i - offset < 0:
-                        continue
-                    cur = dp[i-offset][nexty][nextx]
-                    dp[i-offset][nexty][nextx] = min(dp[i-offset]
-                                                     [nexty][nextx], dp[i][cury][curx] + 1)
-                    if cur != dp[i-offset][nexty][nextx]:
-                        updated = True
-                if updated:
-                    bfs(nextx, nexty)
-        bfs(curx, cury)
-        ans = inf
-        for i in range(k+1):
-            ans = min(ans, dp[i][n-1][m-1])
-        ans = -1 if ans == inf else ans
-        return ans
+                if grid[i][j] == 1 and obstacle < k and (i, j, obstacle + 1) not in visited:
+                    visited.add((i, j, obstacle+1))
+                    q.append((i, j, obstacle+1, path+1))
+                if grid[i][j] == 0 and (i, j, obstacle) not in visited:
+                    if (i, j) == (m-1, n-1):
+                        return path + 1
+                    visited.add((i, j, obstacle))
+                    q.append((i, j, obstacle, path + 1))
+        return -1
 
 
 arr = json.loads(input())
