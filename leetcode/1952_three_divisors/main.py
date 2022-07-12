2d262d4e9326ee3d1f8ce693ddae3399689c41cf
Ryo Ogawa <negibokken@gmail.com>
2022-07-12 19:48:30 +0900

    Add leetcode 1952_three_divisors

--- leetcode/1952_three_divisors/main.py
+++ leetcode/1952_three_divisors/main.py
@@ -2,13 +2,14 @@
 
 from typing import List
 import json
+import math
 from bplib.butil import TreeNode, arr2TreeNode, btreeconnect, aprint
 
 
 class Solution:
     def isThree(self, n: int) -> bool:
         cnt = 1
-        for i in range(1, n//2+1):
+        for i in range(1, int(math.sqrt(n)+1)):
             if (n % i == 0):
                 cnt += 1
         return cnt == 3
