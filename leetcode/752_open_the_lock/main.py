77c0e23ab94473ed8a91efa182096d3923eb05b8
bokken <negibokken@gmail.com>
2022-10-24 22:18:16 +0900

    Add leetcode 752_open_the_lock

--- leetcode/752_open_the_lock/main.py
+++ leetcode/752_open_the_lock/main.py
@@ -9,13 +9,6 @@ import collections
 
 class Solution(object):
     def openLock(self, deadends, target):
-        def neighbors(node):
-            for i in range(4):
-                x = int(node[i])
-                for d in (-1, 1):
-                    y = (x + d) % 10
-                    yield node[:i] + str(y) + node[i+1:]
-
         dead = set(deadends)
         queue = collections.deque([('0000', 0)])
         seen = {'0000'}
@@ -25,10 +18,14 @@ class Solution(object):
                 return depth
             if node in dead:
                 continue
-            for nei in neighbors(node):
-                if nei not in seen:
-                    seen.add(nei)
-                    queue.append((nei, depth+1))
+            for i in range(4):
+                x = int(node[i])
+                for d in (-1, 1):
+                    y = (x + d) % 10
+                    nei = node[:i] + str(y) + node[i+1:]
+                    if nei not in seen:
+                        seen.add(nei)
+                        queue.append((nei, depth+1))
         return -1
 
 
