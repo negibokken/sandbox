4e79241bd4d4e973083a17a33260f66253a1016a
bokken <negibokken@gmail.com>
2022-11-07 20:33:05 +0900

    Add leetcode 1669_merge_in_between_linked_lists

--- leetcode/1669_merge_in_between_linked_lists/main.py
+++ leetcode/1669_merge_in_between_linked_lists/main.py
@@ -5,13 +5,40 @@ import json
 from bplib.butil import TreeNode, arr2TreeNode, btreeconnect, aprint
 
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        return 1
+    def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        prev = None
+        cnt = 0
+        while cnt <= b:
+            if cnt == a:
+
+            cnt += 1
+        return None
 
 
-arr = json.loads(input())
-nodes = list(map(arr2TreeNode, arr))
-root = btreeconnect(nodes)
+list1_raw = json.loads(input())
+list1 = [ListNode(ele) for ele in list1_raw]
+prev = None
+for e in list1:
+    if prev:
+        prev.next = e
+    prev = e
+a = json.loads(input())
+b = json.loads(input())
+list2_raw = json.loads(input())
+list2 = [ListNode(ele) for ele in list2_raw]
+prev = None
+for e in list1:
+    if prev:
+        prev.next = e
+    prev = e
 
 sol = Solution()
+print(sol.mergeInBetween(list1, a, b, list2))
