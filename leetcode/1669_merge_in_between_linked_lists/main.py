b6ba8c145c705f1014b76f4ef5fd8d8c3cc5a70d
bokken <negibokken@gmail.com>
2022-11-09 22:39:44 +0900

    Add leetcode 1669_merge_in_between_linked_lists

--- leetcode/1669_merge_in_between_linked_lists/main.py
+++ leetcode/1669_merge_in_between_linked_lists/main.py
@@ -14,13 +14,32 @@ class ListNode:
 
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        prev = None
+        dummy = ListNode()
+        dummy.next = list1
+        cur, prev = dummy, None
         cnt = 0
-        while cnt <= b:
+        before_a, after_b = None, None
+        while cnt <= b+2:
             if cnt == a:
-
+                before_a = cur
+            elif cnt == b + 2:
+                after_b = cur
+            cur = cur.next
             cnt += 1
-        return None
+        before_a.next = list2
+        cur = list2
+        while cur:
+            prev = cur
+            cur = cur.next
+        prev.next = after_b
+        return dummy.next
+
+
+def printListNode(head):
+    while head:
+        print(head.val)
+        head = head.next
+    print("\n")
 
 
 list1_raw = json.loads(input())
@@ -30,15 +49,22 @@ for e in list1:
     if prev:
         prev.next = e
     prev = e
+# printListNode(list1[0])
 a = json.loads(input())
 b = json.loads(input())
 list2_raw = json.loads(input())
 list2 = [ListNode(ele) for ele in list2_raw]
 prev = None
-for e in list1:
+for e in list2:
     if prev:
         prev.next = e
     prev = e
-
+# printListNode(list2[0])
+# printListNode(list2[0])
 sol = Solution()
-print(sol.mergeInBetween(list1, a, b, list2))
+ans = sol.mergeInBetween(list1[0], a, b, list2[1])
+arr = []
+while ans:
+    arr.append(ans.val)
+    ans = ans.next
+aprint(arr)
